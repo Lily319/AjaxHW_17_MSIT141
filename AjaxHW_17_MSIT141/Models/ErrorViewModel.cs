ab20fff4855fe267a56f94a529c0344884c08b91
Lily319 <leftbehindgd2009@gmail.com>
2022-07-03 16:31:15 +0800

    加入專案檔案。

---
+++ AjaxHW_17_MSIT141/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AjaxHW_17_MSIT141.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
